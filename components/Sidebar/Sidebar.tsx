033f360be79006a9a5d9ef6033770b0b5b23163c
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-11-17 00:37:01 +0700

    sidebar toggle animation

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,31 +1,37 @@
 'use client';
+
 import { SidebarDisplayContext } from '@/components/Globals/Providers/SidebarDisplayProvider';
-import clsx from 'clsx';
+import { motion } from 'framer-motion';
 import { useContext } from 'react';
 
 export const Sidebar = () => {
   const items = Array.from({ length: 100 }, (v, i) => i + 1);
   const { isDisplaySidebar } = useContext(SidebarDisplayContext);
 
+  const animationVariants = {
+    hidden: { left: '-200px' },
+    visible: { left: '0px' },
+  };
+
   return (
-    <nav
-      className={clsx(
-        'fixed',
-        'top-0',
-        'z-10',
-        'h-screen',
-        'overflow-y-auto',
-        'overscroll-contain',
-        'bg-emerald-400',
-        'p-4',
-        'w-[200px]',
-        isDisplaySidebar ? 'flex flex-col' : 'hidden',
-      )}
+    <motion.div
+      className={
+        'top-0 flex w-[200px] flex-col overflow-y-auto overscroll-contain bg-emerald-400'
+      }
+      style={{
+        left: isDisplaySidebar ? '0px' : '-200px',
+        position: isDisplaySidebar ? 'relative' : 'absolute',
+        height: isDisplaySidebar ? 'auto' : '100vh',
+        zIndex: isDisplaySidebar ? 20 : 2,
+        padding: isDisplaySidebar ? '1rem' : 'calc(72px + 1rem) 1rem 1rem 1rem',
+      }}
+      animate={isDisplaySidebar ? 'visible' : 'hidden'}
+      variants={animationVariants}
     >
-      <h1 className="mt-12">Drawer</h1>
+      <h1>Drawer</h1>
       {items.map((item) => {
         return <p key={item}>Menu {item}</p>;
       })}
-    </nav>
+    </motion.div>
   );
 };
